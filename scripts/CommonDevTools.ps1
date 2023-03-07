2e17abdec5d9e73158717e2148d0eafcb3520319
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-07 09:08:23 -0500

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -6,3 +6,5 @@ choco install -y tortoisegit
 choco install -y notepadplusplus
 choco install -y 7zip.install
 choco install -y sysinternals
+choco install -y nodejs-lts
+choco install -y adobereader
