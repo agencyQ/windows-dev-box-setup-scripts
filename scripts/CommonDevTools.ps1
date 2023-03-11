6b22fde70fe478fff7c1247bf0dcbf27a722ad94
Steve Hamilton <steve.hamilton@agencyq.com>
2023-03-11 17:55:23 -0500

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -1,5 +1,6 @@
 
 # tools we expect devs across many scenarios will want
+choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
 choco install -y tortoisegit
 choco install -y notepadplusplus
 choco install -y 7zip.install
